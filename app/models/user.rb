6998e7f2f165cd3a6c760e86b105e9d423cdb736
rezahusain <rezahusain99@gmail.com>
2024-07-01 11:05:23 -0500

    Generated comments

--- app/models/user.rb
+++ app/models/user.rb
@@ -28,4 +28,5 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :own_photos, class_name: "Photo", foreign_key: "owner_id"
+  has_many :comments, foreign_key: "author_id"
 end
