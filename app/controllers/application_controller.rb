4dc9b1436ca053d45fe847a047b707a85683460b
rezahusain <rezahusain99@gmail.com>
2024-07-02 13:31:20 -0500

    Removed authenticate user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,4 @@
 class ApplicationController < ActionController::Base
-  before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   # Allows attributes to be changed within sign up and update forms, keys point to the attributes 
