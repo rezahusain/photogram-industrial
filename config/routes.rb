d2e838b5f83b2a8b2b09e99d041ae7db542aaea8
rezahusain <rezahusain99@gmail.com>
2024-07-02 22:50:13 -0500

    Added navbar and CDN assets

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root "photos#index"
   resources :likes
   resources :follow_requests
   resources :comments
@@ -8,5 +9,5 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-  root "photos#index"
+  
 end
