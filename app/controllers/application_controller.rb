77f19a475130d3a0835beba423b4dda83b400a4b
rezahusain <rezahusain99@gmail.com>
2024-07-03 10:10:36 -0500

    Added alert and notice flash messages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :authenticate_user!
 
   # Allows attributes to be changed within sign up and update forms, keys point to the attributes 
   def configure_permitted_parameters
