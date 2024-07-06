9bb7c017058aedc9f5c634f73a8bdb60551a81de
rezahusain <rezahusain99@gmail.com>
2024-07-05 21:49:42 -0500

    Added liked tab

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,9 @@ Rails.application.routes.draw do
   resources :photos
   devise_for :users
   get ":username/liked" => "users#liked", as: :liked
+  get ":username/feed" => "users#feed", as: :feed
+  get ":username/followers" => "users#followers", as: :followers
+  get ":username/following" => "users#following", as: :following
   get ":username" => "users#show", as: :user
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
