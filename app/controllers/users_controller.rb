1ac1d3e2b848855c13c54dd7edbe678e5ddef63b
rezahusain <rezahusain99@gmail.com>
2024-07-05 22:02:05 -0500

    Created routes and tabbed interfaces for all user features

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,4 +5,13 @@ class UsersController < ApplicationController
   def liked
     @user = User.find_by!(username: params.fetch(:username))
   end
+  def feed
+    @user = User.find_by!(username: params.fetch(:username))
+  end
+  def followers
+    @user = User.find_by!(username: params.fetch(:username))
+  end
+  def following
+    @user = User.find_by!(username: params.fetch(:username))
+  end
 end
